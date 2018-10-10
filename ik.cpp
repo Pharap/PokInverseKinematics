260ba947821845f2508ebc1c348ba440b729efe7
Pharap <2933055+Pharap@users.noreply.github.com>
2018-10-10 10:46:16 +0100

    Improve code style
    Changed code to use C++11 features.
    Changed names to be more descriptive.
    Added Point<T> type.
    Templated Rope to take a size template argument.
    Corrected use of int to std::size_t where appropriate.

--- ik.cpp
+++ ik.cpp
@@ -1,126 +1,220 @@
-#include "Pokitto.h"
-#include <stdint.h>
-Pokitto::Core pok;
+#include <Pokitto.h>
+#include <cstddef>
+#include <cmath>
+#include <array>
 
-static const uint16_t palette[] = {
-0x0, 0xffff, 0x4b44, 0x9f29, 0xf81f, 0xc1c7, 0xc307, 0x4a49, 0x6b4d, 0x7bcf, 0xc5f1, 0x948d, 0x5ae8,
+using Pokitto::Core;
+using Pokitto::Display;
+using Pokitto::Buttons;
+
+static const uint16_t palette[] =
+{
+	0x0000, 0xffff, 0x4B44, 0x9F29, 0xF81F,
+	0xC1C7, 0xC307, 0x4A49, 0x6B4B, 0x7BCF,
+	0xC5F1, 0x948D, 0x5AE8,
 };
 
-int screen = 0;
-int cursorX = 0;
-int cursorY = 0;
+constexpr float pi = 3.14159265359f;
+constexpr float radiansToDegreesFactor = 180.0f / pi;
+constexpr float degreesToRadiansFactor = pi / 180.0f;
 
-float flenDirX(float len, int dir) {
-    float pi = 3.14159265359;
-    dir = ((-dir % 360)+ 360)% 360;
-    float fDir = dir;
-    return len * cos(pi * fDir / 180);
+constexpr float radiansToDegrees(float radians)
+{
+	return radians * radiansToDegreesFactor;
 }
 
-float flenDirY(float len, int dir) {
-    float pi = 3.14159265359;
-    dir = ((-dir % 360)+ 360)% 360;
-    float fDir = dir;
-    return len * sin(pi * fDir / 180);
+constexpr float degreesToRadians(float degrees)
+{
+	return degrees * degreesToRadiansFactor;
 }
 
-float fgetAngle(float x1, float y1, float x2, float y2) {
-    float pi = 3.14159265359;
-    int angle = (atan2(y1 - y2, x1 - x2))*180 / pi;
-    angle = (((-angle) % 360)+ 360)% 360;
-    return angle;
+constexpr int normaliseAngle(int angle)
+{
+	return (((-angle) % 360) + 360) % 360;
 }
 
-class Segment {
-    public:
-    float x;
-    float y;
-    float dir;
-    float index;
+constexpr float normaliseAngle(float angle)
+{
+	return (((-angle) % 360) + 360) % 360;
+}
 
-    Segment(int x_, int y_, int dir_, int i) {
-        x = x_;
-        y = y_;
-        dir = dir_;
-        index = i;
-    }
+constexpr float lengthDirectionX(float length, int direction)
+{
+	return length * std::cos(degreesToRadians(normaliseAngle(direction)));
+}
 
-    Segment(){}
+constexpr float lengthDirectionY(float length, int direction)
+{
+	return length * std::sin(degreesToRadians(normaliseAngle(direction)));
+}
 
-    void drawSegment() {
-        pok.display.drawLine(x, y, x + flenDirX(7, dir), y + flenDirY(7, dir));
-    }
+float getAngle(float x1, float y1, float x2, float y2)
+{
+	return normaliseAngle(radiansToDegrees(std::atan2(y1 - y2, x1 - x2)));
+}
 
-    void moveSegment(float gx, float gy) {
-        dir = fgetAngle(gx, gy, x, y);
+template<typename Type>
+struct Point
+{
+	using ValueType = Type;
 
-        x = gx - flenDirX(8, dir);
-        y = gy + flenDirY(8, -dir);
-    }
+	ValueType x;
+	ValueType y;
+	
+	Point(void) = default;
+	Point(ValueType x, ValueType y)
+		: x(x), y(y)
+	{
+	}
 };
 
-class Rope {
-    public:
-    Segment segments[10];
+class Segment
+{
+private:
+	float x;
+	float y;
+	float direction;
+	
+public:
+	Segment(void) = default;
 
-    Rope(int x_, int y_) {
-        for (int i = 0; i < 10; i++) {
-            segments[i] = Segment(x_, y_, 90, i);
-            y_ -= 8;
-        }
-    }
+	Segment(float x, float y, int direction)
+		: x(x), y(y), direction(direction)
+	{
+	}
 
-    void drawRope() {
-        for (int i = 0; i < 10; i++) {
-            segments[i].drawSegment();
-        }
-    }
+	Segment(Point<int> point, int direction)
+		: Segment(point.x, point.y, direction)
+	{
+	}
+	
+	float getX(void) const
+	{
+		return this->x;
+	}
+	
+	float getY(void) const
+	{
+		return this->y;
+	}
+	
+	float getDirection(void) const
+	{
+		return this->direction;
+	}
 
-    void moveRope(int cx, int cy) {
-        segments[9].moveSegment(cx, cy);
-        for (int i = 8; i >= 0; i--) {
-            segments[i].moveSegment(segments[i+1].x, segments[i+1].y);
-        }
-    }
+	void drawSegment(void)
+	{
+		Display::drawLine(this->x, this->y, this->x + lengthDirectionX(7, dir), this->y + lengthDirectionY(7, dir));
+	}
+
+	void moveSegmentTowards(Point<float> point)
+	{
+		this->moveSegmentTowards(point.x, point.y);
+	}
+
+	void moveSegmentTowards(float x, float y)
+	{
+		this->direction = getAngle(x, y, this->x, this->y);
+
+		this->x = (x - lengthDirectionX(8, this->direction));
+		this->y = (y - lengthDirectionY(8, this->direction));
+	}
 };
 
-void moveCursor() {
-    if (pok.buttons.rightBtn()) {
-        cursorX += 3;
-    }
-    if (pok.buttons.upBtn()) {
-        cursorY -= 3;
-    }
-    if (pok.buttons.leftBtn()) {
-        cursorX -= 3;
-    }
-    if (pok.buttons.downBtn()) {
-        cursorY += 3;
-    }
+template<std::size_t N>
+class Rope
+{
+private:
+	std::array<Segment, N> segments;
+
+public:	
+	Rope(float x, float y)
+	{
+		for (std::size_t i = 0; i < segments.size(); ++i)
+		{
+			segments[i] = Segment(x, y, 90);
+			y -= 8;
+		}
+	}
+	
+	Rope(Point<float> point)
+		: Rope(point.x, point.y)
+	{
+	}
+
+	void drawRope(void)
+	{
+		for (std::size_t i = 0; i < segments.size(); ++i)
+			segments[i].drawSegment();
+	}
+
+	void moveRopeTowards(int x, int y)
+	{
+		segments[0].moveSegmentTowards(x, y);
+		for (std::size_t i = 1; i < segments.size(); ++i)
+			segments[i].moveSegmentTowards(segments[i - 1].getX(), segments[i - 1].getY());
+	}
+};
+
+void moveCursor(Point<float> & cursor)
+{
+	if (Buttons::rightBtn())
+		cursor.x += 3;
+
+	if (Buttons::upBtn())
+		cursor.y -= 3;
+
+	if (Buttons::leftBtn())
+		cursor.x -= 3;
+
+	if (Buttons::downBtn())
+		cursor.y += 3;
 }
 
-void drawCursor() {
-    pok.display.drawPixel(cursorX, cursorY);
+void drawCursor(Point<float> cursor)
+{
+	Display::drawPixel(static_cast<int>(cursor.x), static_cast<int>(cursor.y));
 }
 
-int main() {
-    pok.begin();
-    pok.setFrameRate(30);
-    pok.display.load565Palette(palette);
-    pok.display.bgcolor = 4;
-    pok.display.setInvisibleColor(4);
-    Rope trope = Rope(50, 87);
-    while (pok.isRunning()) {
-        if( pok.update()) {
-            pok.buttons.pollButtons();
-            if (screen == 0) {
-                moveCursor();
-                trope.moveRope(cursorX, cursorY);
-                pok.display.setColor(0);
-                trope.drawRope();
-                pok.display.setColor(1);
-                drawCursor();
-            }
-        }
-    }
+enum class GameState
+{
+	Gameplay,
+};
+
+int main(void)
+{
+	Core::begin();
+	Core::setFrameRate(30);
+	
+	Display::load565Palette(palette);
+	Display::bgcolor = 4;
+	Display::setInvisibleColor(4);
+	
+	GameState gameState = GameState::Gameplay;
+	Rope<10> rope = Rope(50, 87);
+	Point<int> cursor;
+	
+	while (Core::isRunning())
+	{
+		if(!Core::update())
+			continue;
+		
+		Buttons::pollButtons();
+		switch(gameState)
+		{
+			case GameState::Gameplay:
+			{
+				moveCursor(cursor);
+				rope.moveRopeTowards(cursor);
+				
+				Display::setColor(0);
+				rope.drawRope();
+				Display::setColor(1);
+				
+				drawCursor(cursor);
+				break;
+			}
+		}
+	}
 }
